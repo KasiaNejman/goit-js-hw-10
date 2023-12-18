9be395ee26e24795ca3441d04c2186609efe9e88
KasiaNejman <139686065+KasiaNejman@users.noreply.github.com>
2023-12-18 21:16:49 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,38 @@
-import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+import { fetchBreeds, fetchCatByBreed } from "./cat-api.js";
 
-document.addEventListener('DOMContentLoaded', async () => {
-  const breedSelect = document.querySelector('.breed-select');
-  const loader = document.querySelector('.loader');
-  const error = document.querySelector('.error');
-  const catInfo = document.querySelector('.cat-info');
-
+document.addEventListener("DOMContentLoaded", async () => {
+  const breedSelect = document.querySelector(".breed-select");
+  const loader = document.querySelector(".loader");
+  const error = document.querySelector(".error");
+  const catInfo = document.querySelector(".cat-info");
+  breedSelect.style.display = "none";
   const showLoader = () => {
-    loader.style.display = 'block';
-    error.style.display = 'none';
-    catInfo.style.display = 'none';
+    loader.style.display = "block";
+    error.style.display = "none";
+    catInfo.style.display = "none";
   };
 
   const hideLoader = () => {
-    loader.style.display = 'none';
+    loader.style.display = "none";
   };
 
-  const showError = errorMsg => {
+  const showError = (errorMsg) => {
     error.textContent = errorMsg;
-    error.style.display = 'block';
+    error.style.display = "block";
   };
 
   try {
     showLoader();
     const breeds = await fetchBreeds();
     breedSelect.innerHTML = breeds
-      .map(breed => `<option value="${breed.id}">${breed.name}</option>`)
-      .join('');
+      .map((breed) => `<option value="${breed.id}">${breed.name}</option>`)
+      .join("");
     hideLoader();
   } catch (err) {
     showError(err.message);
   }
 
-  breedSelect.addEventListener('change', async () => {
+  breedSelect.addEventListener("change", async () => {
     const selectedBreed = breedSelect.value;
     if (selectedBreed) {
       try {
@@ -44,7 +44,7 @@ document.addEventListener('DOMContentLoaded', async () => {
           <p>Description: ${catData.breeds[0].description}</p>
           <p>Temperament: ${catData.breeds[0].temperament}</p>
         `;
-        catInfo.style.display = 'block';
+        catInfo.style.display = "block";
         hideLoader();
       } catch (err) {
         showError(err.message);
