3c0128d74cabe1bf956a2c433ef3c260d0c55429
KasiaNejman <139686065+KasiaNejman@users.noreply.github.com>
2023-12-18 14:10:53 +0100

    Update cat-api.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,25 +1,25 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api_key =
-  'live_I0ULZh8Io7BHofB8jY65gLfm92pP8zb2r5aZMLig7TBMFzWPab3GfI6fjt5T99M7';
-axios.defaults.headers.common['x-api-key'] = api_key;
+  "live_Jzzp0LqnxrddX7Mlj0V5XyQ6u5bcabYHw7t257YwIJDtTDCz8mQrsxnyHOoQnc6Z";
+axios.defaults.headers.common["x-api-key"] = api_key;
 
 export const fetchBreeds = async () => {
   try {
-    const response = await axios.get('https://api.thecatapi.com/v1/breeds');
-    return response.data.map(breed => ({ id: breed.id, name: breed.name }));
+    const response = await axios.get("https://api.thecatapi.com/v1/breeds");
+    return response.data.map((breed) => ({ id: breed.id, name: breed.name }));
   } catch (err) {
-    throw new Error('Error loading breeds');
+    throw new Error("Error loading breeds");
   }
 };
 
-export const fetchCatByBreed = async breedId => {
+export const fetchCatByBreed = async (breedId) => {
   try {
     const response = await axios.get(
       `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
     );
     return response.data[0];
   } catch (err) {
-    throw new Error('Error loading cat info');
+    throw new Error("Error loading cat info");
   }
 };
