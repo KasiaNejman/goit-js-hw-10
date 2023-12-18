f8bdf32807f30b77218b6b0116d62f6e1380bbfd
KasiaNejman <139686065+KasiaNejman@users.noreply.github.com>
2023-12-18 21:01:16 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ const populateBreeds = () => {
 
   loader.style.display = "block";
   error.style.display = "none";
-  breedSelect.style.display = "none";
 
   fetchBreeds()
     .then((breeds) => {
